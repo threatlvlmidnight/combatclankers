68f768e6ce9f7de31bd674d3d035fd85934be6ca
Micah Holland <goddammitmicah@gmail.com>
2026-03-07 23:04:14 -0600

    feat: scaffold Phaser project

---
+++ game.js
@@ -0,0 +1,13 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 900,
+  height: 650,
+  backgroundColor: '#0d0d1a',
+  physics: {
+    default: 'arcade',
+    arcade: { debug: false, gravity: { x: 0, y: 0 } }
+  },
+  scene: [MenuScene, BattleScene, UIScene]
+};
+
+window.game = new Phaser.Game(config);
